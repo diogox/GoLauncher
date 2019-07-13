40d34487b16308a813798d8cce21d46d3bb28332
Diogo Pinto <diogoxavierpinto@gmail.com>
2019-07-13 22:52:09 +0100

    Fix file browser mode.

--- search/modes/file/root.go
+++ search/modes/file/root.go
@@ -36,12 +36,9 @@ func (fb *FileBrowserMode) IsEnabled(input string) bool {
 }
 
 func (fb *FileBrowserMode) HandleInput(input string) api.Action {
-	// Set global for helper methods (only used to set query from a relative path, really..)
-	_INPUT_ = input
-
 	// Get absolute path
 	absPath, _ := getAbsPath(input)
-	nOfResults := 9 // TODO: Get number of resutls from preference
+	nOfResults := 9 // TODO: Get number of results from preference
 
 	// Not an existing path
 	if !isDir(absPath) {
@@ -51,9 +48,12 @@ func (fb *FileBrowserMode) HandleInput(input string) api.Action {
 	}
 
 	// If '~', add separator to the end
-	if input == "~" {
+	if input == "~" && _INPUT_ != "~/" {
 		return actions.NewSetUserQuery("~/")
 	}
+
+	// Set global for helper methods (only used to set query from a relative path, really..)
+	_INPUT_ = input
 
 	// Otherwise, show results
 	files, err := ioutil.ReadDir(absPath)
@@ -70,7 +70,10 @@ func (*FileBrowserMode) DefaultItems(input string) []api.SearchResult {
 
 func showSuggestions(_path string, nOfResults int) []api.SearchResult {
 	fileName := path.Base(_path)
-	basePath := _path[:len(_path)-len(fileName)]
+	if fileName == "." {
+		fileName = ""
+	}
+	basePath := _path[:len(_path) - len(fileName)]
 
 	files, err := ioutil.ReadDir(basePath)
 	if err != nil {
@@ -100,8 +103,10 @@ func renderResults(files []os.FileInfo, basePath string) []api.SearchResult {
 		itemPath := path.Join(basePath, f.Name())
 
 		// Get relative path, if it was typed by the user
-		fileName := path.Base(_INPUT_)
-		basePath := _INPUT_[:len(_INPUT_)-len(fileName)]
+		userFolder, _ := getAbsPath("~")
+		if strings.HasPrefix(itemPath, userFolder) {
+			itemPath = strings.Replace(itemPath, userFolder, "~", 1)
+		}
 
 		// Create result
 		opts := result.SearchResultOptions{
@@ -109,7 +114,7 @@ func renderResults(files []os.FileInfo, basePath string) []api.SearchResult {
 			Description:      "See what's inside",
 			IconPath:         DIR_ICON,
 			IsDefaultSelect:  false,
-			OnEnterAction:    actions.NewSetUserQuery(path.Join(basePath, f.Name()) + string(os.PathSeparator)),
+			OnEnterAction:    actions.NewSetUserQuery(itemPath + string(os.PathSeparator)),
 			OnAltEnterAction: actions.NewOpen(itemPath),
 		}
 
